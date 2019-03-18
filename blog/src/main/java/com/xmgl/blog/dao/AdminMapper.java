11c10b04c1eadd0a2279f4c502c70b508fea9cf4
LemonGHub <qinping_email@163.com>
2019-03-18 13:10:35 +0800

    测试mybatis-generation

---
+++ blog/src/main/java/com/xmgl/blog/dao/AdminMapper.java
@@ -0,0 +1,17 @@
+package com.xmgl.blog.dao;
+
+import com.xmgl.blog.entity.Admin;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer adminId);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    Admin selectByPrimaryKey(Integer adminId);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
---
+++ blog/src/main/java/com/xmgl/blog/dao/TagMapper.java
@@ -0,0 +1,17 @@
+package com.xmgl.blog.dao;
+
+import com.xmgl.blog.entity.Tag;
+
+public interface TagMapper {
+    int deleteByPrimaryKey(Integer tagId);
+
+    int insert(Tag record);
+
+    int insertSelective(Tag record);
+
+    Tag selectByPrimaryKey(Integer tagId);
+
+    int updateByPrimaryKeySelective(Tag record);
+
+    int updateByPrimaryKey(Tag record);
+}
