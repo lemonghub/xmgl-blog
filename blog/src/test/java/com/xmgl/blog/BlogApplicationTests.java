74fd568fc23f644c791524cd165bfe32813871ce
obsolete <1191430716@qq.com>
2019-04-21 22:18:36 +0800

    11

--- blog/src/test/java/com/xmgl/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/xmgl/blog/BlogApplicationTests.java
@@ -1,16 +1,28 @@
 package com.xmgl.blog;
 
+import com.xmgl.blog.entity.Tag;
+import com.xmgl.blog.service.TagService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogApplicationTests {
 
+    @Autowired
+    private TagService tagService;
+
     @Test
     public void contextLoads() {
+        List<Tag> tags = tagService.selectTagAll();
+        for(Tag e : tags){
+            System.out.println(e.getTagName());
+        }
     }
 
 }
